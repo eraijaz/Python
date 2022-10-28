9220ae1c87036747bec9b982d821b1d1e36523eb
Aijaz ahmad <60282478+eraijaz@users.noreply.github.com>
2022-10-29 01:00:21 +0530

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,6 @@
+print("Datatypes in  python programming")
+print("int()=negative ,0,positive numbers")
+print("float=decimal numbers example(1.5,0.5)")
+print("String =letters,number,special characters ")
+print("boolean=true false")
+
---
+++ typeFuction.py
@@ -0,0 +1,12 @@
+# type() it is used to know the type of data 
+from tkinter import Y
+
+
+x=100
+print("the type of ",x ,"is",type(x))
+y=10.5
+print("the type of", y ,"is",y,type(y))
+z="welcome to python"
+print("the type of ",z," is",type(z))
+my_list=["aijaz,100","java,10"]
+print("the type of ",my_list," is",type(my_list))
